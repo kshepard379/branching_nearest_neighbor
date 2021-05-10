3e19d620a2f08fd548bc353718f7fe38e1f881cc
Kyle Shepard <34076343+kshepard379@users.noreply.github.com>
2021-05-09 21:42:40 -0700

    code that doesn't work

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <vector>
+
+using std::vector;
+using std::cout;
+
+// //aribitrary size graph
+vector<vector<double>> adjMat;
+
+void dead(){
+
+}
+
+void generateMatrix(vector<vector<double>> &mat, int size){
+
+    for(int i = 0; i < size; i++){
+        mat[i][i] = 0.0;
+        for(int j = i + 1; j < size; j++){
+            double rand = 0.5;
+            mat[i][j] = rand;
+            mat[j][i] = rand;
+        }
+    }
+}
 
 int main(){
-    std::cout << "project framework";
+    int size = 5;
+    generateMatrix(adjMat, size);
+
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            cout << adjMat[i][j] << ", ";
+        }
+        cout << "\b\b" << std::endl;
+    }
+
+    std::cout << "Test";
+
     return 0;
 }
